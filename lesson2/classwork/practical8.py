a24a8a2f7c09d68344d1b50345abdc86e690b561
Vlad <43403715+Vlad-Harutyunyan@users.noreply.github.com>
2020-04-09 20:39:59 +0300

    Add files via upload

---
+++ lesson2/classwork/practical8.py
@@ -0,0 +1,10 @@
+def practical8(arr):
+    num = 0
+    for x in arr:
+        if num < x < max(arr):
+            num = x
+        else :
+            num = 0.5
+    return num
+if __name__ == "__main__":
+    print(practical8([5, 5, 5]))
